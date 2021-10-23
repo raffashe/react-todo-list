b55af07917b953b531287fcdda061ec6b139ffb6
Raffaela Silva <raffacst@outlook.com>
2021-10-22 22:34:00 -0300

    em processo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+
 const App = () => {
+  const [list, setlist] = useState<Item[]>([
+    { id: 1, name: 'estudar sempre', done: false},
+    { id: 2, name: 'evoluir todo dia', done: false},
+  ]);
   return(
     <C.Container>
       <C.Area>
-        <C.Header>
-          Lista de tarefas
-        </C.Header>
+        <C.Header>Lista de tarefas</C.Header>
+
+        {/* Area de adicionar nova tarefa */}
+        {list.map((item, index)=>(
+          <div>{item.name}</div>
+        ))}
+
       </C.Area>
     </C.Container>
   );
